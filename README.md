92597fe517a29514267f9bc6762fb84705cc4591
Jon Schlueter <jschluet@redhat.com>
2016-02-23 06:57:55 -0500

    Initial import of concept class

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # shade_janitor
 Utility based on shade to select and cleanup resources from CI runs in a tenant
+
+Intended to automate selecting resouces for janitor cleanup in a cattle only
+automation tenant
+
+That means there are only a couple of restrictions on cleanup:
+
+if no instances are running for network resources they are subject to cleanup
+if an instance is hanginging around for more than 8 hours it is subject to cleanup
+
+don't try to cleanup resources this tenant doesn't control
+don't touch jenkins or slave instances
---
+++ shade_janitor/select_related.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+
+class SelectRelatedResources:
+    """ Helper class to allow you to easily select a group of resources
+
+    this uses a shade openstack_cloud instance to query resources from
+    it stores id and name in a double dictionary with the first level
+    being resource type
+
+    import shade
+    cloud = shade.openstack_cloud(cloud='cloud_name')
+    resources = SelectRelatedResources(cloud)
+
+    resources.select_all_networks()
+    selection = resources.get_selection()
+    for key in selection:
+        print selection[key]
+    """
+
+    def __init__(self, cloud):
+        self._cloud = cloud
+        if self._cloud is None:
+            raise Exception('No cloud provided')
+
+        self._selection = {}
+
+    def _add(self, resource_type, uuid, name=None):
+        if resource_type not in self._selection:
+            self._selection[resource_type] = {}
+        if uuid not in self._selection[resource_type]:
+            entry = {'id': uuid, 'name': name}
+            self._selection[resource_type][uuid] = entry
+
+    def select_instances(self):
+        """
+        Excludes instances with either 'jenkins' or 'slave' in their names
+        """
+        for instance in self._cloud.list_servers():
+            for entry in ['jenkins', 'slave']:
+                if entry in instance.name:
+                    continue
+            self._add('instances', instance.id, instance.name)
+
+    def select_insances_name_substring(self, search_substring):
+        """
+        will select related resources based on provided substring
+
+        Excludes instances with either 'jenkins' or 'slave' in their names
+        """
+        for instance in self._cloud.list_servers():
+            for entry in ['jenkins', 'slave']:
+                if entry in instance.name:
+                    continue
+            if search_substring in instance.name:
+                self._add('instances', instance.id, instance.name)
+
+    def select_networks(self):
+        """
+        Exlcude router:external routers
+        """
+        for network in self._cloud.list_networks():
+            if network['router:external']:
+                continue
+            self._add('nets', network['id'], network['name'])
+
+    def select_networks_name_substring(self, search_substring):
+        """
+        Exlcude router:external routers
+        """
+        for network in self._cloud.list_networks():
+            if network['router:external']:
+                continue
+            if search_substring in network['name']:
+                self._add('nets', network['id'], network['name'])
+
+    def select_subnets(self):
+        for subnet in self._cloud.list_subnets():
+            self._add('subnets', subnet['id'], subnet['name'])
+
+    def select_subnets_name_substring(self, search_substring):
+        for subnet in self._cloud.list_subnets():
+            if search_substring in subnet['name']:
+                self._add('subnets', subnet['id'], subnet['name'])
+
+    def select_routers(self):
+        for router in self._cloud.list_routers():
+            self._add('routers', router['id'], router['name'])
+
+    def select_routers_name_substring(self, search_substring):
+        for router in self._cloud.list_routers():
+            self._add('routers', router['id'], router['name'])
+
+    def select_floatingips(self):
+        for fip in self._cloud.list_floatingips():
+            print fip
+            self._add('fips', fip.id, fip.name)
+
+    def get_selection(self):
+        return self._selection
