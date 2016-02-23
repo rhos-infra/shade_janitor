b6a1b28a352f1dac4d60568c1345fc0acbbe9c87
Jon Schlueter <jschluet@redhat.com>
2016-02-23 15:06:36 -0500

    code simplification in janitor

--- shade_janitor/janitor.py
+++ shade_janitor/janitor.py
@@ -34,6 +34,7 @@ if __name__ == '__main__':
     cloud = shade.openstack_cloud(cloud=args.cloud)
 
     resources = SelectRelatedResources(cloud)
+    cleanup = {}
 
 #    if args.old_instances:
 #        for instance in cloud.server_list():
@@ -72,52 +73,9 @@ if __name__ == '__main__':
 
         cleanup = resources.get_selection()
 
-    pp.pprint(cleanup)
-    cleanup_resources(cloud, cleanup, dry_run=True)
+    if len(cleanup) > 0:
+        pp.pprint(cleanup)
+        cleanup_resources(cloud, cleanup, dry_run=True)
 
-    if args.run_cleanup:
-        cleanup_resources(cloud, cleanup, dry_run=False)
-
-#    if args.run_cleanup:
-#        nova = auth['nova']
-#        if 'instances' in cleanup:
-#            instance_list = nova.servers.list()
-#            for uuid in cleanup['instances']:
-#                if cleanup['instances'][uuid] is not None and 'name' in cleanup['instances'][uuid]:
-#                    for instance in instance_list:
-#                        if instance.id == uuid:
-#                            print "attempting to delete {} instance".format(instance.id)
-#                            instance.delete()
-#
-#        neutron = auth['neutron']
-#        if 'ports' in cleanup:
-#            for uuid in cleanup['ports']:
-#                if cleanup['ports'][uuid] is not None and 'name' in cleanup['ports'][uuid]:
-#                    print 'attempting port-delete {}'.format(uuid)
-#                    neutron.delete_port(uuid)
-#
-#        if 'router_interfaces' in cleanup and False:
-#            for uuid in cleanup['router_interfaces']:
-#                rec = cleanup['router_interfaces'][uuid]
-#                router_map_list = neutron.list_routers()
-#                for router in router_map_list['routers']:
-#                    print router
-#                    neutron.remove_interface_router(router,rec['subnet_id'])
-#
-#        if 'subnets' in cleanup:
-#            for uuid in cleanup['subnets']:
-#                if cleanup['subnets'][uuid] is not None and 'name' in cleanup['subnets'][uuid]:
-#                    print 'attempting subnet-delete {}'.format(uuid)
-#                    neutron.delete_subnet(uuid)
-#
-#        if 'nets' in cleanup:
-#            for uuid in cleanup['nets']:
-#                if cleanup['nets'][uuid] is not None and 'name' in cleanup['nets'][uuid]:
-#                    print 'attempting net-delete {}'.format(uuid)
-#                    neutron.delete_network(uuid)
-#
-#        if 'routers' in cleanup:
-#            for uuid in cleanup['routers']:
-#                if cleanup['routers'][uuid] is not None and 'name' in cleanup['routers'][uuid]:
-#                    print 'attempting router-delete {}'.format(uuid)
-#                    neutron.delete_router(uuid)
+        if args.run_cleanup:
+            cleanup_resources(cloud, cleanup, dry_run=False)
