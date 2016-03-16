f60b73ccf273969be27555ed2fe010e07f0eaaf5
Arie Bregman <abregman@redhat.com>
2016-03-16 10:09:46 +0200

    Change SelectRelatedResources to Resources
    SelectRelatedResources is really all about Resources.
    
    It is not only selecting them, but also checking if different
    conditions exist.
    
    Tests changed accordingly.

--- test/test_resources.py
+++ test/test_resources.py
@@ -1,52 +1,52 @@
-import unittest
-from shade_janitor.resources import SelectRelatedResources
 from mock import Mock
+import unittest
 
+from shade_janitor.resources import Resources
 
 class TestResources(unittest.TestCase):
 
     def test_resources_fails_no_cloud(self):
         with self.assertRaises(Exception):
-            SelectRelatedResources(None)
+            Resources(None)
 
     def test_blacklist_normal(self):
-        r = SelectRelatedResources(Mock())
+        r = Resources(Mock())
         instance = Mock()
         instance.name = 'abcde-rdo-ci-88-foo'
         self.assertFalse(r.check_instance_blacklisted(instance))
 
     def test_blacklist_jenkins(self):
-        r = SelectRelatedResources(Mock())
+        r = Resources(Mock())
         instance = Mock()
         instance.name = 'jenkins-helper'
         self.assertTrue(r.check_instance_blacklisted(instance))
 
     def test_blacklist_slave(self):
-        r = SelectRelatedResources(Mock())
+        r = Resources(Mock())
         instance = Mock()
         instance.name = 'private-slave'
         self.assertTrue(r.check_instance_blacklisted(instance))
 
     def test_blacklist_slave_2(self):
-        r = SelectRelatedResources(Mock())
+        r = Resources(Mock())
         instance = Mock()
         instance.name = 'slave-helper'
         self.assertTrue(r.check_instance_blacklisted(instance))
 
     def test_blacklist_rpm_mirror(self):
-        r = SelectRelatedResources(Mock())
+        r = Resources(Mock())
         instance = Mock()
         instance.name = 'rpm-mirror-01'
         self.assertTrue(r.check_instance_blacklisted(instance))
 
     def test_is_permanent_non_perm_instance(self):
-        r = SelectRelatedResources(Mock())
+        r = Resources(Mock())
         instance = Mock()
         instance.name = 'abcde-rdo-ci-88-foo'
         self.assertFalse(r.is_permanent(instance))
 
     def test_is_permanent_perm_instance(self):
-        r = SelectRelatedResources(Mock())
+        r = Resources(Mock())
         instance = Mock()
         instance.name = 'permanent-rdo-ci-88-foo'
         self.assertTrue(r.is_permanent(instance))
