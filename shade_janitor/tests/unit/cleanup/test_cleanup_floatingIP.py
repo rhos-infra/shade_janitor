bbf272a6721b4b7ded34017460073ad538761df8
Arie Bregman <abregman@redhat.com>
2016-03-20 12:41:40 +0200

    Add unit tests for cleanup
    Test the different resources cleanup.
    
    autopep8 cleanup of whitespace issues
    
    prune unused imports in tests

--- shade_janitor/tests/unit/cleanup/test_cleanup_floatingIP.py
+++ shade_janitor/tests/unit/cleanup/test_cleanup_floatingIP.py
@@ -57,3 +57,22 @@ class TestCleanupFloatingIP(base.BaseTestCase):
             self.cloud, self.resources.get_selection(), dry_run=True)
 
         self.assertFalse(self.cloud.delete_floating_ip.called)
+
+    @mock.patch('shade_janitor.cleanup.dry_cleanup_floating_ips')
+    def test_dry_cleanup_floatingIP_micro(self, mock_floatingIP_cleanup):
+        self.resources._add_floatingip(self.fip)
+        cleanup.cleanup_resources(self.cloud, self.resources.get_selection())
+        self.assertTrue(mock_floatingIP_cleanup.called)
+
+    @mock.patch('shade_janitor.cleanup.cleanup_floating_ips')
+    def test_cleanup_floatingIP_micro(self, mock_floatingIP_cleanup):
+        dry_cleanup = False
+        self.resources._add_floatingip(self.fip)
+        cleanup.cleanup_resources(
+            self.cloud, self.resources.get_selection(), dry_cleanup)
+        self.assertTrue(mock_floatingIP_cleanup.called)
+
+    @mock.patch('shade_janitor.cleanup.dry_cleanup_floating_ips')
+    def test_cleanup_no_floatingIP_micro(self, mock_floatingIP_cleanup):
+        cleanup.cleanup_resources(self.cloud, self.resources.get_selection())
+        self.assertFalse(mock_floatingIP_cleanup.called)
--- shade_janitor/tests/unit/cleanup/test_cleanup_network.py
+++ shade_janitor/tests/unit/cleanup/test_cleanup_network.py
@@ -30,3 +30,22 @@ class TestCleanupNetwork(base.BaseTestCase):
     def test_cleanup_no_network(self, mock_show_cleanup):
         cleanup.cleanup_resources(self.cloud, self.resources.get_selection())
         self.assertFalse(mock_show_cleanup.called)
+
+    @mock.patch('shade_janitor.cleanup.dry_cleanup_networks')
+    def test_dry_cleanup_network_micro(self, mock_networks_cleanup):
+        self.resources._add('nets', self.network.id, self.network.name)
+        cleanup.cleanup_resources(self.cloud, self.resources.get_selection())
+        self.assertTrue(mock_networks_cleanup.called)
+
+    @mock.patch('shade_janitor.cleanup.cleanup_networks')
+    def test_cleanup_network_micro(self, mock_networks_cleanup):
+        dry_cleanup = False
+        self.resources._add('nets', self.network.id, self.network.name)
+        cleanup.cleanup_resources(
+            self.cloud, self.resources.get_selection(), dry_cleanup)
+        self.assertTrue(mock_networks_cleanup.called)
+
+    @mock.patch('shade_janitor.cleanup.dry_cleanup_networks')
+    def test_cleanup_no_network_micro(self, mock_networks_cleanup):
+        cleanup.cleanup_resources(self.cloud, self.resources.get_selection())
+        self.assertFalse(mock_networks_cleanup.called)
