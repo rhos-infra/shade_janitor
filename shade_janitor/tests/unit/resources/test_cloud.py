f3fef3e3d2d3214cf2294d9dad426e4d620d6647
Jon Schlueter <jschluet@redhat.com>
2016-03-22 07:33:53 -0400

    rework test_cloud to use base
    also included positive test case as well

--- shade_janitor/tests/unit/resources/test_cloud.py
+++ shade_janitor/tests/unit/resources/test_cloud.py
@@ -1,3 +1,4 @@
+import mock
 from unittest import TestCase
 
 from shade_janitor.resources import NoCloudException
@@ -9,3 +10,6 @@ class TestResourcesCloud(TestCase):
     def test_resources_fails_no_cloud(self):
         with self.assertRaises(NoCloudException):
             Resources(None)
+
+    def test_passes_with_something(self):
+        Resources(mock.Mock())
